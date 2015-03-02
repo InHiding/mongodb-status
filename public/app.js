376abbc45d12e4c8777906798918f9d31606508a
Giuliano Melo <giuliano.melo@abril.com.br>
2015-03-02 20:35:49 -0300

    change to mongoDB stats

--- public/app.js
+++ public/app.js
@@ -3,38 +3,44 @@ $(function() {
   function timestamp() { return (new Date).getTime() / 1000; }
 
   var cluster = document.querySelector("#cluster");
-  cluster.toFixed = function(value, precision) {
-    return Number(value).toFixed(precision);
+  cluster.stats = {};
+  cluster.toMB = function(value, precision) {
+    if(precision === null || precision === undefined) {
+      precision = 2;
+    }
+
+    var megabyte = 1024*1024;
+    return Number(value/megabyte).toFixed(precision);
   };
 
   var chart = $("#chart").epoch({
     type: "time.line",
     axes: ["left", "bottom"],
     data: [
-      {label: "Writes", values: [{time: timestamp(), y: 0}]},
-      {label: "Reads", values: [{time: timestamp(), y: 0}]}
+      {label: "Storage Size", values: [{time: timestamp(), y: 0}]},
+      {label: "Indexes Size", values: [{time: timestamp(), y: 0}]}
     ]
   });
 
   var socket = io.connect();
 
   socket.on("stats", function(data) {
-    cluster.stats = data.new_val.query_engine;
+    cluster.stats = data;
     chart.push([
-      { time: timestamp(), y: cluster.stats.written_docs_per_sec },
-      { time: timestamp(), y: cluster.stats.read_docs_per_sec}
+      { time: timestamp(), y: cluster.stats.dataSize },
+      { time: timestamp(), y: cluster.stats.indexSize}
     ]);
   });
 
-  socket.on("servers", function(data) {
-    if (data.length)
-      return cluster.servers = data;
+  // socket.on("collections", function(data) {
+  //   if (data.length)
+  //     return cluster.collections = data;
 
-    if (!data.old_val)
-      return cluster.servers.push(data.new_val);
+  //   if (!data.old_val)
+  //     return cluster.collections.push(data.new_val);
 
-    for (var s in cluster.servers)
-      if (cluster.servers[s].id == data.old_val.id)
-        cluster.servers[s] = data.new_val;
-  });
+  //   for (var s in cluster.collections)
+  //     if (cluster.collections[s].id == data.old_val.id)
+  //       cluster.collections[s] = data.new_val;
+  // });
 });
